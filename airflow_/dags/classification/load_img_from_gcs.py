e0e44e411b2fa7ab9482399b112d44b8bb20cdae
juheon <ojhgold@gmail.com>
2023-01-26 23:10:13 +0900

    fix: dest path

--- airflow_/dags/classification/load_img_from_gcs.py
+++ airflow_/dags/classification/load_img_from_gcs.py
@@ -3,7 +3,7 @@ import sys
 
 
 def download_img_from_gcs(scraped_time, bucket, site, keyword):
-    dest = f"/opt/ml/final-project-level3-cv-06/airflow_/dags/classification/data/{keyword}/{site}/{scraped_time}"
+    dest = f"/opt/ml/final-project-level3-cv-06/airflow_/dags/classification/data/{keyword}/{site}/"
     os.makedirs(dest, exist_ok=True)
     bash_script = (
         f"gsutil -m cp -r gs://{bucket}/{keyword}/{site}/{scraped_time}/ {dest}"
