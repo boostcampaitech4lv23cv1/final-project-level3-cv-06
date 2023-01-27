b342ba22afcdc9d074b5b9436cc786ac18909fa3
oh <ojhgold@gmail.com>
2023-01-27 15:48:29 +0900

    feat: modifyed UI

--- vue-project/src/views/SelectView.vue
+++ vue-project/src/views/SelectView.vue
@@ -1,19 +1,106 @@
 <template>
   <v-app class="hero">
     <v-container height="100%">
-      <v-row class="justify-center mt-16">
-
-        <logo :style="{ height: '150px', width: '600px' }" />
+      <v-row class="justify-center">
+        <logo :style="{ height: '35vh', width: '35vw' }" />
       </v-row>
-      <v-row class="buttons">
-        <v-col cols="6" class="d-flex justify-center align-end">
-          <mode :style="{ height: '50px', width: '150px' }" />
-        </v-col>
-        <v-col cols="6" class="d-flex justify-center align-end">
-          <category :style="{ height: '50px', width: '230px' }" />
+         <v-row class="d-flex justify-center align-center buttons mt-16">
+        <v-btn rounded variant="plain" color="transparent" @click="infoChange">
+          <description :style="{ height: '80px', width: '180px' }" />
+        </v-btn>
+      </v-row>
+      <v-row class="justify-center mt-16" v-show="showInfo == 1">
+        <v-col cols="auto">
+          <v-card height="250">
+            <v-toolbar
+              color="black"
+              :style="{ 'font-size': '30px', color: 'black' }"
+            >
+              <v-icon>mdi-information-outline</v-icon></v-toolbar
+            >
+
+            <v-card-text :style="{ 'font-size': '20px', color: 'black' }">
+              태일이는 내일 로봇 그림 경진대회에 참가할 예정입니다.<br /><br />
+              작품 출품 전 미리 로봇의 성능을 테스트 하기 위해, 로봇이 제시어에
+              맞는 이미지를 생성하는지 확인해야 합니다.<br /><br />
+              물감이 부족한 태일이를 도와,
+              <u>생성되는 그림을 보고 제시어를 빨리 맞혀주세요!</u>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                :style="{ background: 'gray' }"
+                outlined
+                id="click"
+                background="red"
+                backgroundColor="black"
+                @click="infoChange"
+                >Ok</v-btn
+              >
+              <v-spacer></v-spacer>
+
+              <v-btn
+                :style="{ background: 'gray' }"
+                class="red"
+                outlined
+                id="click"
+                background="red"
+                @click="movePage('/description', { page: 4 })"
+              >
+                Learn how to play
+              </v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
         </v-col>
       </v-row>
       <v-row>
+        <v-col cols="4" class = "text-center">
+          Animals
+        </v-col>
+
+        <v-col cols="4" class = "text-center">
+          Celebs        </v-col>
+
+        <v-col cols="4" class = "text-center">
+          Landmarks
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="4">
+          <v-img
+            src="../assets/tiger.jpg"
+            max-height="30vh"
+  max-width="10vw"
+            class="mx-auto"
+            @click="changeCategory(entertainer)"
+            />
+        </v-col>
+
+        <v-col cols="4">
+          <v-img
+            src="../assets/200.jpg"
+
+            max-height="30vh"
+  max-width="10vw"
+            class="mx-auto"
+            @click="changeCategory(entertainer)"
+            />
+        </v-col>
+
+        <v-col cols="4">
+          <v-img
+            src="../assets/tower.jpg"
+            max-height="30vh"
+  max-width="10vw"
+            class="mx-auto"
+            @click="changeCategory(entertainer)"
+            />
+        </v-col>
+
+      
+      </v-row>
+      <!-- <v-row>
         <v-col cols="6">
           <v-radio-group v-model="selectedMode">
             <v-btn
@@ -50,7 +137,7 @@
             </v-btn>
           </v-radio-group>
         </v-col>
-      </v-row>
+      </v-row> -->
       <v-col cols="12" class="d-flex justify-center mt-16">
         <v-btn :disabled="categoryIsEmpty" color="yellow" @click="startGame"
           >Game start</v-btn
@@ -68,8 +155,7 @@ import category from "../svg/categoryView.vue";
 export default {
   components: {
     logo,
-    mode,
-    category,
+
   },
   data() {
     return {
@@ -82,6 +168,7 @@ export default {
       selectedMode: "painttransformer",
 
       modeItems: [{ text: "Paint Transformer", value: "painttransformer" }],
+      showInfo: false,
     };
   },
   computed: {
@@ -101,6 +188,13 @@ export default {
     },
     changeCategory(value) {
       this.selectedCategory = value;
+    },
+    infoChange() {
+      if (this.showInfo == true) {
+        this.showInfo = false;
+      } else {
+        this.showInfo = true;
+      }
     },
   },
 };
