821e4be4f8292abcfabae3302b09dbd392438fa2
sshinohs <sshinohs@gmail.com>
2023-01-31 18:05:12 +0900

    add: paint_transformer_setup

---
+++ backend/PaintTransformer/inference_main.py
@@ -0,0 +1,27 @@
+import sys
+
+from PaintTransformer.inference import *
+from PIL import Image
+
+import os
+
+resize_l = 1024
+K = 5
+stroke_num = 8
+patch_size = 32
+
+model, meta_brushes, device = init(stroke_num, model_path="model.pth")
+
+image = Image.open("dataset/original/animal/cat/cat_1.jpg")
+output = inference(
+    image=image,
+    resize_l=resize_l,  # resize original input to this size. (max(w, h) = resize_l)
+    K=K,  # set K
+    device=device,
+    model=model,
+    meta_brushes=meta_brushes,
+    stroke_num=stroke_num,
+    patch_size=patch_size,
+)
+
+output[-1].save("cat_1_result.jpg")
