86ed06e3a17e1f6bff5f776cc9af55f70af80da1
nask <tjdrms0459@naver.com>
2023-01-31 23:09:32 +0900

    feat: add BGM

--- vue-project/src/views/DemoResult.vue
+++ vue-project/src/views/DemoResult.vue
@@ -1,34 +1,58 @@
 <template>
   <v-app class="fill-height hero">
-    <v-container>
-      <v-row class=" justify-center">
+    <v-container height="100%">
+      <v-row class="justify-center">
         <result :style="{ height: '15vh', margin: '0vh 0vw 5vh 0vw' }" />
+      </v-row>
+      <v-row class="d-flex justify-end" :style="{ margin: '3vh 0vw 5vh 0vw' }">
+        <v-btn rounded variant="plain" @click="audioChange" height="5vh">
+          <v-icon icon="mdi-volume-high" size="5vh" v-if="audioInfo == false">
+          </v-icon>
+          <v-icon icon="mdi-volume-off" size="5vh" v-if="audioInfo == true">
+          </v-icon>
+        </v-btn>
       </v-row>
 
       <v-row>
-        <v-col v-for="n in 9" :key="n" class="d-flex child-flex no-padding" cols="4">
-          <v-img :width="'45vw'" :height="'60vh'" :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
-            aspect-ratio="1" cover :src="`data:image/gif;base64,${originImg[n - 1]}`" class="grey lighten-2"
-            @click="moveDetail(n - 1)">
+        <v-col
+          v-for="n in 9"
+          :key="n"
+          class="d-flex child-flex no-padding"
+          cols="4"
+        >
+          <v-img
+            :width="'45vw'"
+            :height="'60vh'"
+            :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+            aspect-ratio="1"
+            cover
+            :src="`data:image/gif;base64,${originImg[n - 1]}`"
+            class="grey lighten-2"
+            @click="moveDetail(n - 1)"
+          >
           </v-img>
         </v-col>
       </v-row>
 
-
       <v-dialog v-model="showDialog" max-height="75vh" max-width="100vw">
         <v-row>
-          <v-col cols="4" class='mx-auto text-center nums'>
+          <v-col cols="4" class="mx-auto text-center nums">
             {{ answer[dialogNum] }}
           </v-col>
         </v-row>
         <v-row class="d-flex justify-center align-center">
-          <v-col cols=4 class='d-flex justify-center align-center'>
-            <v-img :src="`data:image/gif;base64,${resultImg[dialogNum]}`" max-height="50vh" />
+          <v-col cols="4" class="d-flex justify-center align-center">
+            <v-img
+              :src="`data:image/gif;base64,${resultImg[dialogNum]}`"
+              max-height="50vh"
+            />
           </v-col>
         </v-row>
         <v-row>
           <v-col cols="1" class="mx-auto">
-            <v-btn color="primary" block @click="showDialog = false">Close</v-btn>
+            <v-btn color="primary" block @click="showDialog = false"
+              >Close</v-btn
+            >
           </v-col>
         </v-row>
       </v-dialog>
@@ -37,7 +61,7 @@
 </template>
 
 <script>
-import result from '../svg/resultText.vue'
+import result from "../svg/resultText.vue";
 export default {
   components: {
     result,
@@ -49,27 +73,36 @@ export default {
       showDialog: false,
       dialogNum: false,
       path: this.$store.state.imgPath,
-      answer: this.$store.state.answerList
+      answer: this.$store.state.answerList,
+      audioInfo: !this.$root.audio.muted,
     };
   },
   methods: {
     moveDetail(index) {
-      this.dialogNum = index
-      this.showDialog = true
+      this.dialogNum = index;
+      this.showDialog = true;
+    },
+    audioChange() {
+      this.$root.audio.muted = !this.$root.audio.muted;
+      if (this.audioInfo == true) {
+        this.audioInfo = false;
+      } else {
+        this.audioInfo = true;
+      }
     },
   },
   async mounted() {
-    console.log(this.answer)
-    console.log(this.path)
+    console.log(this.answer);
+    console.log(this.path);
     let response = await this.$api(
-      'http://127.0.0.1:8000/api/v1/game/result',
+      "http://127.0.0.1:8000/api/v1/game/result",
       "POST",
       { paths: this.path }
     );
 
     this.originImg = response.origin_imgs;
     this.resultImg = response.result_imgs;
-  }
+  },
 };
 </script>
 
@@ -87,13 +120,13 @@ export default {
 }
 
 @font-face {
-  font-family: 'answer';
-  src: url('../fonts/Lobster-Regular.ttf')
+  font-family: "answer";
+  src: url("../fonts/Lobster-Regular.ttf");
 }
 
 .nums {
-  font-family: 'answer';
+  font-family: "answer";
   font-size: 2.3rem;
-  color: white
+  color: white;
 }
 </style>
