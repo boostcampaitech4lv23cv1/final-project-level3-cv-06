57ed8453ced35ffd428d3151db7b839d8dcb5d89
tec10182 <tec10182@naver.com>
2023-02-02 00:05:13 +0900

    add: demoresult responsive

--- vue-project/src/views/DemoResult.vue
+++ vue-project/src/views/DemoResult.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app class="fill-height hero">
-    <v-container>
+    <v-container v-if="!isPortrait">
       <v-row class=" justify-center">
         <result :style="{ height: '15vh', margin: '0vh 0vw 5vh 0vw' }" />
       </v-row>
@@ -33,6 +33,40 @@
         </v-row>
       </v-dialog>
     </v-container>
+
+    <v-container v-if="isPortrait">
+      <v-row class=" justify-center">
+        <result :style="{ height: '10vh', margin: '3vh 0vw 10vh 0vw' }" />
+      </v-row>
+
+      <v-row>
+        <v-col v-for="n in 9" :key="n" class="d-flex child-flex no-padding" cols="4">
+          <v-img :width="'30vw'" :height="'20vh'" :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+            aspect-ratio="1" cover :src="`data:image/gif;base64,${originImg[n - 1]}`" class="grey lighten-2"
+            @click="moveDetail(n - 1)">
+          </v-img>
+        </v-col>
+      </v-row>
+
+
+      <v-dialog v-model="showDialog" max-height="75vh" max-width="100vw">
+        <v-row>
+          <v-col cols="4" class='mx-auto text-center nums'>
+            {{ answer[dialogNum] }}
+          </v-col>
+        </v-row>
+        <v-row class="d-flex justify-center align-center">
+          <v-col cols="9" class='d-flex justify-center align-center'>
+            <v-img :src="`data:image/gif;base64,${resultImg[dialogNum]}`" max-height="50vh" />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="3" class="mx-auto">
+            <v-btn color="primary" block @click="showDialog = false">Close</v-btn>
+          </v-col>
+        </v-row>
+      </v-dialog>
+    </v-container>
   </v-app>
 </template>
 
@@ -49,15 +83,20 @@ export default {
       showDialog: false,
       dialogNum: false,
       path: this.$store.state.imgPath,
-      answer: this.$store.state.answerList
+      answer: this.$store.state.answerList,
+      isPortrait: true
     };
   },
   methods: {
+    checkOrientation() {
+      this.isPortrait = window.screen.orientation.type === "portrait-primary";
+    },
     moveDetail(index) {
       this.dialogNum = index
       this.showDialog = true
     },
   },
+  
   async mounted() {
     console.log(this.answer)
     console.log(this.path)
@@ -69,7 +108,12 @@ export default {
 
     this.originImg = response.origin_imgs;
     this.resultImg = response.result_imgs;
-  }
+    this.checkOrientation();
+    window.addEventListener("orientationchange", this.checkOrientation);
+  },
+  beforeUnmount() {
+    window.removeEventListener("orientationchange", this.checkOrientation);
+  },
 };
 </script>
 
