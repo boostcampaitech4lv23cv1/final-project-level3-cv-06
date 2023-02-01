1b5161cafdddf59adf88dbc4470bb0ae8929f6dc
nask <tjdrms0459@naver.com>
2023-02-01 12:09:03 +0900

    refactor: add v-container for sound icon position

--- vue-project/src/views/LeaderBoard.vue
+++ vue-project/src/views/LeaderBoard.vue
@@ -1,36 +1,38 @@
 <template>
   <v-app class="hero">
-    <v-row class="d-flex justify-center">
-      <ranking :style="{ height: '15vh', margin: '10vh 0vw 0vh 0vw' }" />
-    </v-row>
-    <v-row class="d-flex justify-end" :style="{ margin: '3vh 0vw 0vh 0vw' }">
-      <v-btn rounded variant="plain" @click="audioChange" height="5vh">
-        <v-icon icon="mdi-volume-high" size="5vh" v-show="audioInfo == false">
-        </v-icon>
-        <v-icon icon="mdi-volume-off" size="5vh" v-show="audioInfo == true">
-        </v-icon>
-      </v-btn>
-    </v-row>
-    <v-row class="d-flex justify-center font">
-      <v-col cols="4">
-        <div class="scroll">
-          <v-table class="table">
-            <thead>
-              <tr>
-                <th class="text-center">Name</th>
-                <th class="text-center">Score</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="item in ranking" :key="item.name">
-                <td class="text-center">{{ item.name }}</td>
-                <td class="text-center">{{ item.score }}</td>
-              </tr>
-            </tbody>
-          </v-table>
-        </div>
-      </v-col>
-    </v-row>
+    <v-container height="100%">
+      <v-row class="d-flex justify-center">
+        <ranking :style="{ height: '15vh', margin: '10vh 0vw 0vh 0vw' }" />
+      </v-row>
+      <v-row class="d-flex justify-end" :style="{ margin: '3vh 0vw 0vh 0vw' }">
+        <v-btn rounded variant="plain" @click="audioChange" height="5vh">
+          <v-icon icon="mdi-volume-high" size="5vh" v-show="audioInfo == false">
+          </v-icon>
+          <v-icon icon="mdi-volume-off" size="5vh" v-show="audioInfo == true">
+          </v-icon>
+        </v-btn>
+      </v-row>
+      <v-row class="d-flex justify-center font">
+        <v-col cols="4">
+          <div class="scroll">
+            <v-table class="table">
+              <thead>
+                <tr>
+                  <th class="text-center">Name</th>
+                  <th class="text-center">Score</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="item in ranking" :key="item.name">
+                  <td class="text-center">{{ item.name }}</td>
+                  <td class="text-center">{{ item.score }}</td>
+                </tr>
+              </tbody>
+            </v-table>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
