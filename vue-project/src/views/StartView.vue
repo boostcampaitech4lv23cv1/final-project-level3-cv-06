f5e79b9b660f60451bafda92a8f106d1c0913c5a
kms <tec10182@naver.com>
2023-02-02 13:18:39 +0900

    feat: complete startview & responsive

--- vue-project/src/views/StartView.vue
+++ vue-project/src/views/StartView.vue
@@ -1,26 +1,25 @@
 <template>
   <v-app class="hero">
     <v-container height="100%">
+
+
       <v-row class="d-flex justify-center">
+        <!-- save paint! 로고 출력 -->
         <v-col cols="10" class="d-flex justify-center">
           <logo :style="{ height: '14vh', margin: '10vh 0vw 0vh 0vw' }" />
         </v-col>
       </v-row>
-      <v-row class="d-flex justify-end" :style="{ margin: '3vh 0vw 0vh 0vw' }">
-        <v-btn rounded variant="plain" @click="audioChange" height="5vh">
-          <v-icon icon="mdi-volume-high" size="5vh" v-if="audioInfo == true">
-          </v-icon>
-          <v-icon icon="mdi-volume-off" size="5vh" v-if="audioInfo == false">
-          </v-icon>
-        </v-btn>
-      </v-row>
 
-      <v-row :style="{ margin: '15vh 0vw 0vh 0vw' }">
+
+      <v-row :style="{ margin: '20vh 0vw 0vh 0vw' }">
+        <!-- 게임 버튼 select 페이지로 이동  -->
         <v-col cols="6" class="d-flex justify-center">
           <v-btn rounded variant="plain" color="transparent" @click="movePage('/select')">
             <game :style="{ height: '7vh' }" />
           </v-btn>
         </v-col>
+
+        <!-- 이미지 변환 페이지로 이동하는 버튼 -->
         <v-col cols="6" class="d-flex justify-center">
           <v-btn rounded variant="plain" color="transparent" @click="movePage('/transform')">
             <transform :style="{ height: '7vh' }" />
@@ -28,7 +27,9 @@
         </v-col>
       </v-row>
 
+
       <v-row :style="{ margin: '10vh 0vw 0vh 0vw' }" class="d-flex justify-center">
+        <!-- 리더보드로(랭킹) 이동하는 버튼 -->
         <v-col cols="6" class="d-flex justify-center">
           <v-btn rounded variant="plain" color="transparent" @click="movePage('/leaderboard')">
             <ranking :style="{ height: '7vh' }" />
@@ -39,6 +40,7 @@
   </v-app>
 </template>
 
+
 <script>
 import logo from "../svg/logoView.vue";
 import transform from "../svg/transformButton2.vue";
@@ -46,11 +48,6 @@ import game from "../svg/GameButton.vue";
 import ranking from "../svg/RankingLogo.vue";
 
 export default {
-  data() {
-    return {
-      audioInfo: !this.$root.audio.muted && !this.$root.audio.paused,
-    };
-  },
   components: {
     logo,
     transform,
@@ -58,23 +55,15 @@ export default {
     ranking,
   },
   methods: {
-    movePage(route, query) {
+    movePage(route) {
       this.$router.push({
         path: route,
-        query: query,
       });
-    },
-    audioChange() {
-      if (this.$root.audio.paused) {
-        this.$root.audio.autoplay();
-      } else {
-        this.$root.audio.muted = !this.$root.audio.muted;
-      }
-      this.audioInfo = !this.audioInfo;
     },
   },
 };
 </script>
+
 
 <style scoped>
 .hero {
