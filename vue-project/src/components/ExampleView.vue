bd0c65a489c5c32f15a3bf7f4b5c82f01b12466e
nask <tjdrms0459@naver.com>
2023-02-02 20:49:48 +0900

    feat: added white space & modified UI

--- vue-project/src/components/ExampleView.vue
+++ vue-project/src/components/ExampleView.vue
@@ -3,16 +3,12 @@
     <v-overlay v-model="overlay" width="100%" height="100%" persistent>
       <v-overlay v-model="overlay" width="100%" height="100%" persistent>
         <v-overlay v-model="overlay" width="100%" height="100%" persistent>
-          <v-container
-            class="fill-height fill-width"
-            @click.stop
-            :style="{ position: 'relative' }"
-          >
+          <v-container>
             <v-row class="d-flex justify-center nums">
               <div
                 v-if="page == 2"
                 :style="{
-                  'font-size': '7vh',
+                  'font-size': '5vw',
                   color: 'white',
                 }"
                 class="text-center"
@@ -22,7 +18,7 @@
               <div
                 v-else
                 :style="{
-                  'font-size': '7vh',
+                  'font-size': '5vw',
                   color: 'black',
                 }"
                 class="text-center"
@@ -68,58 +64,104 @@
                   v-if="page == 3"
                   class="mx-auto"
                   src="../assets/bird.gif"
-                  height="40vh"
-                  width="40vw"
+                  height="25vh"
+                  width="25vw"
                 />
                 <v-img
                   v-if="page != 3"
                   class="mx-auto"
                   src="../assets/bird_overlayed.jpg"
-                  height="40vh"
-                  width="40vw"
+                  height="25vh"
+                  width="25vw"
                 />
               </v-col>
               <v-col cols="4" class="mx-auto">
-                <div v-if="page == 6">
-                  <v-progress-linear
-                    class="overlayedbar"
-                    height="20vh"
-                    color="white"
-                    v-model="totalTimer"
-                  >
-                  </v-progress-linear>
-                </div>
-
-                <div v-if="page != 6">
-                  <v-progress-linear
-                    class="overlayedbar"
-                    height="20vh"
-                    color="grey"
-                    v-model="totalTimer"
-                  >
-                  </v-progress-linear>
-                </div>
+                <v-progress-linear
+                  v-if="page == 7"
+                  class="bar"
+                  height="20vh"
+                  color="white"
+                  v-model="totalTimer"
+                >
+                </v-progress-linear>
+                <v-progress-linear
+                  v-if="page != 7"
+                  class="overlayedbar"
+                  height="20vh"
+                  color="grey"
+                  v-model="totalTimer"
+                >
+                </v-progress-linear>
               </v-col>
             </v-row>
-            <v-row>
-              <v-spacer></v-spacer>
-              <v-col cols="4">
-                <div v-if="page == 4">
-                  <v-img
-                    class="mx-auto"
-                    src="../assets/img_timer.jpg"
-                    width="25vw"
-                  />
-                </div>
-                <div v-else class="mx-auto">
-                  <v-img
-                    class="mx-auto"
-                    src="../assets/img_timer_overlayed.jpg"
-                    width="25vw"
-                  />
-                </div>
+            <v-row class="d-flex justify-center text-center">
+              <v-col>
+                <v-progress-circular
+                  v-if="page == 4"
+                  height="5vh"
+                  color="#0000FF"
+                  :size="65"
+                  :width="8"
+                  model-value="100"
+                >
+                  <div
+                    :style="{
+                      'font-size': '3vh',
+                      color: 'black',
+                    }"
+                  >
+                    {{ 12 }}
+                  </div>
+                </v-progress-circular>
+
+                <v-progress-circular
+                  v-if="page != 4"
+                  height="5vh"
+                  color="#1E3269"
+                  :size="65"
+                  :width="8"
+                  model-value="100"
+                >
+                  <div
+                    :style="{
+                      'font-size': '3vh',
+                      color: 'black',
+                    }"
+                  >
+                    {{ 12 }}
+                  </div>
+                </v-progress-circular>
               </v-col>
-              <v-spacer></v-spacer>
+            </v-row>
+
+            <v-row
+              class="d-flex justify-center"
+              :style="{ 'margin-top': '3vh' }"
+            >
+              <div v-if="page == 5" style="display: flex">
+                <v-sheet
+                  v-for="i in 3"
+                  :key="{ i }"
+                  color="white"
+                  elevation="1"
+                  height="7vh"
+                  width="6vh"
+                  rounded
+                  :style="{ 'margin-left': '1vw' }"
+                ></v-sheet>
+              </div>
+              <div v-if="page != 5" style="display: flex">
+                <v-sheet
+                  v-for="i in 3"
+                  :key="{ i }"
+                  color="grey"
+                  elevation="1"
+                  height="7vh"
+                  width="6vh"
+                  rounded
+                  :style="{ 'margin-left': '1vw' }"
+                ></v-sheet>
+              </div>
             </v-row>
 
             <v-row
@@ -128,7 +170,7 @@
             >
               <v-col cols="4">
                 <v-text-field
-                  v-if="page == 5"
+                  v-if="page == 6"
                   readonly
                   bg-color="white"
                   label="answer"
@@ -136,7 +178,7 @@
                   density="compact"
                 ></v-text-field>
                 <v-text-field
-                  v-if="page != 5"
+                  v-if="page != 6"
                   readonly
                   label="answer"
                   single-line
@@ -146,10 +188,10 @@
             </v-row>
             <div
               v-if="page == 1"
-              :style="{ 'font-size': '3vh', color: 'white' }"
+              :style="{ 'font-size': '2vh', color: 'white' }"
               class="text-center"
             >
-              게임 화면의 구성을 설명합니다.<br />
+              게임 화면의 구성을 설명할게요.<br />
               <v-btn
                 class="ma-2"
                 icon="mdi-arrow-right-bold"
@@ -159,10 +201,10 @@
             </div>
             <div
               v-if="page == 2"
-              :style="{ 'font-size': '3vh', color: 'white', position: '' }"
+              :style="{ 'font-size': '2vh', color: 'white', position: '' }"
               class="text-center"
             >
-              1.현재 진행중인 게임 라운드입니다.<br />
+              1.현재 진행중인 게임 라운드예요.<br />
               <v-btn
                 class="ma-2"
                 icon="mdi-arrow-left-bold"
@@ -178,10 +220,10 @@
             </div>
             <div
               v-if="page == 3"
-              :style="{ 'font-size': '3vh', color: 'white', position: '' }"
+              :style="{ 'font-size': '2vh', color: 'white', position: '' }"
               class="text-center"
             >
-              2.현재 라운드에 해당하는 이미지를 생성합니다.<br />
+              2.현재 라운드에 해당하는 이미지가 생성돼요.<br />
               <v-btn
                 class="ma-2"
                 icon="mdi-arrow-left-bold"
@@ -197,10 +239,11 @@
             </div>
             <div
               v-if="page == 4"
-              :style="{ 'font-size': '3vh', color: 'white' }"
+              :style="{ 'font-size': '2vh', color: 'white' }"
               class="text-center"
             >
-              3. 이미지의 완성도입니다.<br />
+              3. 현재 라운드의 남은 시간이에요.<br />제한시간 내에 정답을 맞히지
+              못하면<br />다음 이미지로 넘어가요!<br />
               <v-btn
                 class="ma-2"
                 icon="mdi-arrow-left-bold"
@@ -216,10 +259,10 @@
             </div>
             <div
               v-if="page == 5"
-              :style="{ 'font-size': '3vh', color: 'white' }"
+              :style="{ 'font-size': '2vh', color: 'white' }"
               class="text-center"
             >
-              4. 정답을 입력할 수 있는 칸입니다.<br />
+              4. 정답이 몇 글자인지 알려줘요.<br />
               <v-btn
                 class="ma-2"
                 icon="mdi-arrow-left-bold"
@@ -235,18 +278,37 @@
             </div>
             <div
               v-if="page == 6"
-              :style="{ 'font-size': '3vh', color: 'white' }"
+              :style="{ 'font-size': '2vh', color: 'white' }"
               class="text-center"
             >
-              5. 남은 물감의 총량입니다.<br />
-              그림을 그리면서 물감이 소모되며, 소진 시 미션 실패로 게임이
-              종료됩니다.<br />
-
+              5. 정답을 입력할 수 있는 칸이에요.<br />
               <v-btn
                 class="ma-2"
                 icon="mdi-arrow-left-bold"
                 variant="text"
                 @click="changeQuery(5)"
+              />
+              <v-btn
+                class="ma-2"
+                icon="mdi-arrow-right-bold"
+                variant="text"
+                @click="changeQuery(7)"
+              />
+            </div>
+
+            <div
+              v-if="page == 7"
+              :style="{ 'font-size': '2vh', color: 'white' }"
+              class="text-center"
+            >
+              6. 남은 물감의 총량이에요.<br />
+              그림을 그리면서 물감이 소모되고,<br />
+              소진 시 미션 실패로 게임이 종료돼요.<br />
+              <v-btn
+                class="ma-2"
+                icon="mdi-arrow-left-bold"
+                variant="text"
+                @click="changeQuery(6)"
               />
             </div>
           </v-container>
