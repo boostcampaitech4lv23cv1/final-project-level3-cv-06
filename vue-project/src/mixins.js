7ffe5272b7055ef9ae4e87169609904f5e0a335d
kms <tec10182@naver.com>
2023-02-03 22:45:58 +0900

    refactor: jsDoc

--- vue-project/src/mixins.js
+++ vue-project/src/mixins.js
@@ -1,6 +1,13 @@
 import axios from 'axios';
 export default{
     methods:{
+        /**
+         * 서버와 json 형식으로 통신하기 위한 함수
+         * @function $api
+         * @param {String} url 통신할 서버의 url
+         * @param {String} method GET,POST등 통신 방식
+         * @param {Tuple} data 서버에 보낼 데이터
+         */
         async $api(url,method,data={}){
             return (await axios({
                 method: method,
@@ -14,6 +21,13 @@ export default{
         },
 
 
+        /**
+         * 서버와 form 데이터 형식으로 통신하기 위한 함수
+         * @function $api2
+         * @param {String} url 통신할 서버의 url
+         * @param {String} method GET,POST등 통신 방식
+         * @param {formData} data 서버에 보낼 데이터
+         */
         async $api2(url,method,data){
             return (await axios({
                 method: method,
