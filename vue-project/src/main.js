ab8d0229f3214298d24a103ac4a5ce1671e8afd5
kms <tec10182@naver.com>
2023-02-03 15:18:26 +0900

    delete: unused icon package

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -4,7 +4,6 @@ import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
 import router from "./router";
 import mixins from "./mixins";
-import myIcon from "./myIcon";
 import store from "./store/store";
 import bgm from "./assets/BGM.mp3";
 loadFonts();
@@ -18,10 +17,6 @@ const app = createApp(App);
 app.config.globalProperties.audio = new Audio(bgm);
 app.use(router);
 app.use(store);
-app.use(vuetify, {
-  icons: {
-    "my-icon": myIcon,
-  },
-});
+app.use(vuetify, {});
 app.mixin(mixins);
 app.mount("#app");
