7037be16483994408b556310bc5b8459cd79353d
nask <tjdrms0459@naver.com>
2023-02-08 13:30:33 +0900

    feat: changed size of the trophy icon

--- vue-project/src/svg/fisrtTrophy.vue
+++ vue-project/src/svg/fisrtTrophy.vue
@@ -3,11 +3,9 @@
     xmlns="http://www.w3.org/2000/svg"
     xmlns:xlink="http://www.w3.org/1999/xlink"
     enable-background="new 0 0 64 64"
-    height="64px"
     id="Icons"
     version="1.1"
     viewBox="0 0 64 64"
-    width="64px"
     xml:space="preserve"
   >
     <g id="cup">
